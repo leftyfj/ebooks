821ccc3b5dfbdc7507afc65949b50c402010e760
hisa <gwall59@gmail.com>
2026-07-03 22:02:37 +0900

    トップページを実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,144 @@
-import Image from "next/image";
-
+'use client';
+import '@/styles/globals.scss';
+import '@/styles/top.scss';
+import Image from 'next/image';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    return (
+        <>
+            <header className="header">
+                <div className="header__logo ">
+                    <AutoStoriesIcon fontSize="large" />
+                    <span className="header__logo-main">eBooks</span>
+                    <span className="header__logo-sub">Library Manager</span>
+                </div>
+                <div className="header__login login-test">
+                    <a href="">Googleでログイン</a>
+                </div>
+            </header>
+            <main className="main">
+                <div className="hero">
+                    <div className="hero__inner">
+                        <h1 className="hero__title">
+                            あなたの電子書籍を一元管理<span>eBooks</span>
+                        </h1>
+                        <p className="hero__message">
+                            Kindle・楽天Kobo・BOOK☆WALKER・hontoなど
+                            <br />
+                            複数サービスの電子書籍をまとめて管理できます。
+                        </p>
+                    </div>
+                    <Image
+                        className="hero__image"
+                        src="/inaki-del-olmo-NIJuEQw0RKg-unsplash.jpg"
+                        alt=""
+                        width={4000}
+                        height={1200}
+                        priority
+                    />
+                </div>
+                <section className="section service">
+                    <div className="section__inner">
+                        <h2 className="section__title service__title">
+                            このアプリでできること
+                        </h2>
+                        <div className="service__cards cards">
+                            <div className="card">
+                                <div className="card__img">
+                                    <Image
+                                        src="/books.jpg"
+                                        alt=""
+                                        width={300}
+                                        height={250}
+                                        className="card__image"
+                                    />
+                                </div>
+                                <h3 className="card__title">蔵書管理</h3>
+                                <p className="card__desc">
+                                    電子書籍をまとめて管理
+                                </p>
+                            </div>
+                            <div className="card">
+                                <div className="card__img">
+                                    <Image
+                                        src="/tags.jpg"
+                                        alt=""
+                                        width={300}
+                                        height={250}
+                                        className="card__image"
+                                    />
+                                </div>
+                                <h3 className="card__title">タグ管理</h3>
+                                <p className="card__desc">
+                                    電子書籍に好きなタグを付けて整理
+                                </p>
+                            </div>
+                            <div className="card">
+                                <div className="card__img">
+                                    <Image
+                                        src="/bookstore.jpg"
+                                        alt=""
+                                        width={300}
+                                        height={250}
+                                        className="card__image"
+                                    />
+                                </div>
+                                <h3 className="card__title">書庫管理</h3>
+                                <p className="card__desc">
+                                    電子書籍サービスごとに分類
+                                </p>
+                            </div>
+                            <div className="card">
+                                <div className="card__img">
+                                    <Image
+                                        src="/search.jpg"
+                                        alt=""
+                                        width={300}
+                                        height={250}
+                                        className="card__image"
+                                    />
+                                </div>
+                                <h3 className="card__title">検索</h3>
+                                <p className="card__desc">
+                                    タイトル・著者・タグで電子書籍を検索
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className="section ebook--service">
+                    <div className="section__inner">
+                        <h2 className="section__title ebook--service__title">
+                            対応電子書籍サービス
+                        </h2>
+                        <div className="ebook__logos logos">
+                            <div className="logo">
+                                <MenuBookIcon fontSize="large" />
+                                <span>Kindle</span>
+                            </div>
+                            <div className="logo">
+                                <MenuBookIcon fontSize="large" />
+                                <span>Kobo</span>
+                            </div>
+                            <div className="logo">
+                                <MenuBookIcon fontSize="large" />
+                                <span>BOOK☆WALKER</span>
+                            </div>
+                            <div className="logo">
+                                <MenuBookIcon fontSize="large" />
+                                <span>honto</span>
+                            </div>
+                            <div className="logo">
+                                <MenuBookIcon fontSize="large" />
+                                <span>その他</span>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+            <footer className="footer">@2026 eBooks</footer>
+        </>
+    );
 }
